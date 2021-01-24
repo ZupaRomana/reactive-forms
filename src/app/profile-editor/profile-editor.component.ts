c2db74101ea56c5693d7a70449a5be2f4fd82116
ZupaRomana <35366482+ZupaRomana@users.noreply.github.com>
2021-01-24 15:21:19 +0100

    Add form validators

--- src/app/profile-editor/profile-editor.component.ts
+++ src/app/profile-editor/profile-editor.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
-import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import {AbstractControl, FormArray, FormBuilder, ValidationErrors, ValidatorFn, Validators} from '@angular/forms';
+import {Observable, of} from 'rxjs';
+import {HttpClient} from '@angular/common/http';
+import {catchError, debounceTime, distinctUntilChanged, first, map, switchMap, tap} from 'rxjs/operators';
 
 @Component({
   selector: 'app-profile-editor',
@@ -8,9 +11,9 @@ import {FormArray, FormBuilder, FormControl, FormGroup, Validators} from '@angul
 })
 export class ProfileEditorComponent implements OnInit {
   profileForm = this.fb.group({
-    firstName: [''],
+    firstName: ['', [this.forbiddenNameValidator(/bob/i)]],
     lastName: [''],
-    email: ['', [Validators.required, Validators.email]],
+    email: ['', [Validators.required, Validators.email], [this.validateAsyncEmail.bind(this)]],
     address: this.fb.group({
       street: [''],
       city: [''],
@@ -19,9 +22,9 @@ export class ProfileEditorComponent implements OnInit {
     aliases: this.fb.array([
       this.fb.control('')
     ])
-  });
+  }, {validators: sameNamesValidator});
 
-  constructor(private fb: FormBuilder) { }
+  constructor(private fb: FormBuilder, private http: HttpClient) { }
 
   get aliases(): FormArray {
     return this.profileForm.get('aliases') as FormArray;
@@ -42,6 +45,42 @@ export class ProfileEditorComponent implements OnInit {
   }
 
   onSubmit(): void {
-    console.log(this.profileForm.value);
+    // Check emails on submit click solution
+    // this.http.get('http://localhost:3000/emails').pipe(
+    //   map((emails: string[]) => {
+    //     const emailExists = emails.includes(this.profileForm.get('email').value);
+    //     this.profileForm.get('email').setErrors(emailExists ? {emailTaken: true} : null);
+    //     return emailExists;
+    //   })
+    // ).subscribe(errorExists => {
+    //   if(errorExists) {
+    //     this.router.navigate(['/dashboard']);
+    //   }
+    // });
+  }
+
+  validateAsyncEmail(control: AbstractControl): Observable<ValidationErrors | null> {
+    return control.valueChanges.pipe(
+      debounceTime(500),
+      distinctUntilChanged(),
+      switchMap(() => this.http.get('http://localhost:3000/emails')),
+      map((emails: string[]) => emails.includes(control.value) ? {emailTaken: true} : null),
+      catchError(() => of(null)),
+      first()
+    );
+  }
+
+  private forbiddenNameValidator(regExp: RegExp): ValidatorFn {
+    return (control: AbstractControl): {[key: string]: any} | null => {
+      const forbidden = regExp.test(control.value);
+      return forbidden ? {forbiddenName: {value: `You can't be Bob`}} : null;
+    };
   }
 }
+
+const sameNamesValidator: ValidatorFn = (control: AbstractControl): ValidationErrors | null => {
+  const firstName = control.get('firstName');
+  const lastName = control.get('lastName');
+
+  return firstName.value && lastName.value && firstName.value === lastName.value ? { sameNames: true } : null;
+};
